e2a8bfe8534fd0d5f0e17c471118b316282ce9e0
Kimuu76 <officialkimutai@gmail.com>
2025-03-04 21:29:19 +0300

    made changes

--- server/models/index.js
+++ server/models/index.js
@@ -1,43 +1,41 @@
-'use strict';
+/** @format */
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+//const { Sequelize } = require("sequelize");
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = require("../database");
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+const Product = require("./Product");
+const Purchase = require("./Purchases");
+const Sales = require("./Sales");
+const SalesReturns = require("./SalesReturns");
+const PurchaseReturns = require("./PurchaseReturns");
+const Supplier = require("./Supplier");
+const Company = require("./Company");
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+// ✅ Define relationships (Ensure correct order)
+Product.hasMany(Sales, { foreignKey: "productId" });
+Sales.belongsTo(Product, { foreignKey: "productId" });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+Product.hasMany(Purchase, { foreignKey: "productId" }); // ✅ Fixed missing association
+Purchase.belongsTo(Product, { foreignKey: "productId" });
 
-module.exports = db;
+Product.hasMany(SalesReturns, { foreignKey: "productId", onDelete: "CASCADE" });
+SalesReturns.belongsTo(Product, { foreignKey: "productId" });
+
+Product.hasMany(PurchaseReturns, { foreignKey: "productId" });
+PurchaseReturns.belongsTo(Product, { foreignKey: "productId" });
+
+// ✅ Supplier-Purchase Relationship
+Supplier.hasMany(Purchase, { foreignKey: "supplierId" });
+Purchase.belongsTo(Supplier, { foreignKey: "supplierId" });
+
+module.exports = {
+	Sales,
+	Product,
+	Purchase,
+	Supplier,
+	SalesReturns,
+	PurchaseReturns,
+	Company,
+	sequelize,
+};
