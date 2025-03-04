5c6f72dfe9dec34358e4cfb10e839c7400114df1
Kimuu76 <officialkimutai@gmail.com>
2025-03-04 21:01:20 +0300

    made changes

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,8 @@
-/** @format 
+/** @format */
 
-const { Sequelize } = require("sequelize");
+//const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-	dialect: "sqlite",
-	storage: "./database.sqlite", // SQLite file
-});*/
+const sequelize = require("../database");
 
 const Product = require("./Product");
 const Purchase = require("./Purchases");
